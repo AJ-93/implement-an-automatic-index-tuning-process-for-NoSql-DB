b54f24862a7f2caf6164bde59138179c818fc385
Ajay Kumar Chadayan <ajay.chadayan@st.ovgu.de>
2022-11-16 01:52:09 +0100

    Update initial_test.py

--- initial_test.py
+++ initial_test.py
@@ -1,12 +1,34 @@
 from pymongo import MongoClient
+import requests
+from requests.auth import HTTPDigestAuth
 
 conn = MongoClient("mongodb+srv://ajaykumarchadayan:Intelcoreiat3@cluster0.px5g5ok.mongodb.net/?retryWrites=true&w=majority")
 
 database = conn.get_database('sample_mflix')
 
 
-result = database.comments.find({})
+result = database.comments.find({'name': 'Teresa Thomas'})
+#for comm in result:
+   # print(comm)
 
-query_result = database.comments.find({}).explain()
+# Drop index
+database.restaurants.drop_index('name_1')
 
+query_result = database.comments.find({'name': 'Teresa Thomas'}).explain()
 print(query_result['executionStats'])
+# 'executionTimeMillis': 25, 'totalKeysExamined': 0, 'totalDocsExamined': 41079, 'executionStages': {'stage': 'COLLSCAN'
+
+database.comments.create_index('name')
+index_info = database.comments.index_information()
+print(index_info)
+
+query_result_afterIndex = database.comments.find({'name': 'Teresa Thomas'}).explain()
+print(query_result_afterIndex['executionStats'])
+
+
+index_info = database.comments.index_information()
+print(index_info)
+
+index_stats = database.comments.aggregate( [ {"$indexStats": { } } ] )
+for index in index_stats:
+    print(index)
