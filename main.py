d34e9d2eff409b3ccaab5bce19ec4bc036424408
Ajay Kumar Chadayan <ajay.chadayan@st.ovgu.de>
2023-03-26 03:23:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,30 +10,29 @@ filter = {"op": "query", "millis": {"$gt": 0}}
 conn = MongoClient(CONNECTION_STRING)
 
 database = conn.get_database(DATABASE)
+collection = database[COLLECTION]
 COLLECTION_SYS = database.system.profile
 
 database.command('profile', 1, filter)
+document = collection.find_one()
+fields_of_collection = list(document.keys())
+print (fields_of_collection)
 
 list_slow_queries = COLLECTION_SYS.find(filter)
 
 potential_index_list = []
 i=0
 for query in list_slow_queries:
-    print(query)
+    #print(query)
     index = query['command']['filter']
-    potential_index = list(index.keys())
+    potential_index = index.keys()
+    potential_index = ' '.join(potential_index)
     query_plan = query['planSummary']
     docs_scanned = query['docsExamined']
     docs_returned = query['nreturned']
     index_keys_used = query['keysExamined']
     if index is not None:
-        if query_plan == 'COLLSCAN' :
-            if index_keys_used == 0:
+        if (query_plan == 'COLLSCAN') and (potential_index in fields_of_collection):
                     potential_index_list.append(potential_index)
-
-print (potential_index_list)
-
-
-
 
 
