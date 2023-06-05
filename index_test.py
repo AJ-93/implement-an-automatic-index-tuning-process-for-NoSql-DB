ceb83b1e4d19e17e6957c580443b0263d70464c8
Ajay Kumar Chadayan <ajay.chadayan@st.ovgu.de>
2023-06-05 02:09:33 +0200

    completed index impact experiment

--- index_test.py
+++ index_test.py
@@ -11,10 +11,18 @@ db.command('profile', 1, filter)
 # Insert documents without index
 operation_start_time = time.time()
 for i in range(1000000):
-    collection.insert_one({'column1': f'field{i}', 'column2': i+10, 'column3': True, 'column4': i * 0.01, 'column5': f'2022-01-0{i % 30 + 1}', 'column6': ['A', 'B', 'C'] })
+    collection.insert_one({'column1': f'field{i+20000000}', 'column2': i+70, 'column3': True, 'column4': i * 3.860, 'column5': f'2023-01-0{i % 30 + 1}', 'column6': ['A', 'D','E','B','G','H','C','F'] })
     print(f'records inserted {i}')
 operation_end_time = time.time()
-print("Time taken to insert 1 million documents with 1 index: ", operation_end_time - operation_start_time, "seconds")
+total_time = operation_end_time - operation_start_time
+f =open("time_rec.txt","a")
+f.write("\nTime taken to insert 1 million documents with 12 indexes:" + str(total_time)+ "seconds\n")
+f.close()
+
+for i in range(1000000):
+    collection.delete_one({'column2': i+70})
+    print(f'records deleted {i}')
+
 
 # # Create index
 # collection.create_index("field1")
